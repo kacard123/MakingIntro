17c154699ebb04a387f607c07995da7788b8642a
LimJuYeong <shalvah623@naver.com>
2022-06-22 19:27:03 +0900

    20220622_last

--- Intro animation/Assets/03. Scripts/MainStory.cs
+++ Intro animation/Assets/03. Scripts/MainStory.cs
@@ -9,6 +9,6 @@ public class MainStory : MonoBehaviour
     void OnEnable()
     {
         // Only specifying the sceneName or SceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("Intro",LoadSceneMode.Single);   
+        SceneManager.LoadScene("Main_Room_01",LoadSceneMode.Single);   
     }
 }
